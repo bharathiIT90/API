c8d19e1fda527d3cb867e47baf0c77a7484d97f7
Bharathi <bharathiranganathan24@gmail.com>
2016-07-22 12:53:26 -0700

    Adding Serializers - product and variation

--- products/serializers.py
+++ products/serializers.py
@@ -1,10 +1,34 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Product, Variation 
+
+class VariationSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Variation
+		fields = [
+			"title",
+			"price",
+		]
+
+class ProductSerializer(serializers.ModelSerializer):
+	variation_set = VariationSerializer(many=True,read_only = True)
+	image = serializers.SerializerMethodField()
+	class Meta:
+		model = Product
+		fields = [
+			"id",
+			"title",
+			"image",
+			"variation_set",
+		]
+
+	def get_image(self,obj):
+		return obj.productimage_set.first().image.url
 
 
 class CategorySerializer(serializers.ModelSerializer):
 	url = serializers.HyperlinkedIdentityField(view_name = 'category_detail_api')
+	product_set = ProductSerializer(many=True)
 	class Meta:
 		model = Category
 		fields = [
@@ -12,5 +36,7 @@ class CategorySerializer(serializers.ModelSerializer):
 			"id",
 			"title",
 			"description",
+			"product_set",
+			#"default_category",
 		]
 
